5b945ae59e5a31cd8191c28a646d148fdfce639e
Steve Crow <steve.crow@vonage.com>
2019-03-21 12:36:48 -0400

    Update testing in case developers want to write them.

--- src/test/java/com/nexmo/starter/AppTest.java
+++ src/test/java/com/nexmo/starter/AppTest.java
@@ -3,12 +3,13 @@
  */
 package com.nexmo.starter;
 
+import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    @Test
+    @Ignore
+    public void testAppHasAGreeting() {
+        // Empty Test
     }
 }
